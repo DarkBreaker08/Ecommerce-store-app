f2a5ef7f588d0a2b73f50b5706c62cc02356c071
Gregory <grzesb1508@wp.pl>
2025-03-29 07:38:57 +0100

    Added hamburger menu mechanism to Header component

--- src/Components/Home/Header/Header.tsx
+++ src/Components/Home/Header/Header.tsx
@@ -1,15 +1,30 @@
 import { Link } from "@tanstack/react-router";
 import logo from "../../../assets/logo.png";
 import styles from "./styles.module.scss";
+import { useState } from "react";
 
 export const Header = () => {
+  const [isHamburgerActive, setIsHamburgerActive] = useState<boolean>(false);
+
+  const toogleHamburger = () => {
+    setIsHamburgerActive((prev) => !prev);
+  };
+
+  const handleHamburgerMenuIconChange = () => {
+    if (isHamburgerActive) return <i className="far fa-times"></i>;
+    return <i className="fas fa-outdent"></i>;
+  };
+
   return (
     <section className={styles.header}>
       <a href="#">
         <img src={logo} alt="shop logo" />
       </a>
 
-      <ul className={styles.header__navbar}>
+      <ul
+        className={styles.header__navbar}
+        style={isHamburgerActive ? { right: "0px" } : { right: "-300px" }}
+      >
         <li className={styles.header__navbar__li}>
           <Link to="." className={styles.header__navbar__li__link}>
             Home
@@ -41,11 +56,12 @@ export const Header = () => {
           </Link>
         </li>
       </ul>
-      <div className={`${styles.mobile}`}>
+      <div className={`${styles.mobile}`} onClick={toogleHamburger}>
         <Link to="." className={styles.header__navbar__li__link}>
           <i className="far fa-shopping-bag"></i>
         </Link>
-        <i className="fas fa-outdent"></i>
+
+        {handleHamburgerMenuIconChange()}
       </div>
     </section>
   );
