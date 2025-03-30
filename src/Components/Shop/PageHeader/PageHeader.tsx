3be23e9ba475989d02f3edbffb5b8e543079451b
Gregory <grzesb1508@wp.pl>
2025-03-30 10:18:45 +0200

    Created PageHeader component and its styles

--- src/Components/Shop/PageHeader/PageHeader.tsx
+++ src/Components/Shop/PageHeader/PageHeader.tsx
@@ -3,8 +3,10 @@ import styles from "./styles.module.scss";
 export const PageHeader = () => {
   return (
     <section className={styles.pageheader}>
-      <h2>#stayhome</h2>
-      <h4>Save more with coupons & up to 70% off!</h4>
+      <h1 className={styles.pageheader__title}>#stayhome</h1>
+      <h4 className={styles.pageheader__text}>
+        Save more with coupons & up to 70% off!
+      </h4>
     </section>
   );
 };
--- src/Components/Shop/Shop.tsx
+++ src/Components/Shop/Shop.tsx
@@ -1,10 +1,13 @@
 import { Header } from "../GeneralComponents/Header/Header";
+import { FeaturedProducts } from "../Home/FeaturedProducts/FeaturedProducts";
+import { PageHeader } from "./PageHeader/PageHeader";
 
 export const Shop = () => {
   return (
     <>
       <Header />
-      <h2>sasdasd</h2>
+      <PageHeader />
+      <FeaturedProducts />
     </>
   );
 };
