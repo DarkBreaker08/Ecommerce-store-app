3205404717f357c83e653cc074e13ab894a7d943
Gregory <grzesb1508@wp.pl>
2025-04-03 20:29:39 +0200

    Created first blog page

--- src/Components/Blog/FirstBlogPage/FirstBlogPage.tsx
+++ src/Components/Blog/FirstBlogPage/FirstBlogPage.tsx
@@ -1,5 +1,19 @@
+import { Footer } from "../../GeneralComponents/Footer/Footer";
+import { Header } from "../../GeneralComponents/Header/Header";
+import { Newsletter } from "../../GeneralComponents/Newsletter/Newsletter";
+import { Pagination } from "../../GeneralComponents/Pagination/Pagination";
 import { BlogHeader } from "../BlogHeader/BlogHeader";
+import { FirstBlogSection } from "./FirstBlogSection/FirstBlogSection";
 
 export const FirstBlogPage = () => {
-  return <BlogHeader />;
+  return (
+    <>
+      <Header />
+      <BlogHeader />
+      <FirstBlogSection />
+      <Pagination first="." second="/secondBlogPage" />
+      <Newsletter />
+      <Footer />
+    </>
+  );
 };
