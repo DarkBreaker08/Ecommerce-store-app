2e116fd0ae9ff86795f2e3f646c9e91c27f4f327
Gregory <grzesb1508@wp.pl>
2025-04-03 17:27:49 +0200

    Added API to NewArrivals component

--- src/Components/Home/NewArrivals/NewArrivals.tsx
+++ src/Components/Home/NewArrivals/NewArrivals.tsx
@@ -1,4 +1,4 @@
-import { Product } from "../../GeneralComponents/Product/Product";
+import { Product2 } from "../../GeneralComponents/Product/Product2";
 import product1 from "../../../assets/products/n1.jpg";
 import product2 from "../../../assets/products/n2.jpg";
 import product3 from "../../../assets/products/n3.jpg";
@@ -8,77 +8,27 @@ import product6 from "../../../assets/products/n6.jpg";
 import product7 from "../../../assets/products/n7.jpg";
 import product8 from "../../../assets/products/n8.jpg";
 import styles from "./styles.module.scss";
+import { useGetProductsQuery } from "../../../queries/useGetProductsQuery";
+import { Navigate } from "@tanstack/react-router";
 
 export const NewArrivals = () => {
+  const { data: products } = useGetProductsQuery();
+
+  if (!products) return <Navigate to="." />;
+
   return (
     <section className={styles.newArrivals}>
       <h2>New Arrivals</h2>
       <p>Summer Collection New Modern Design</p>
       <div className={styles.newArrivals__productsContainer}>
-        <Product
-          description={{
-            brand: "adidas",
-            name: "Cartoon Astronaut T-Shirts",
-            price: 78,
-          }}
-          productImgSource={product1}
-        />
-        <Product
-          description={{
-            brand: "adidas",
-            name: "Cartoon Astronaut T-Shirts",
-            price: 78,
-          }}
-          productImgSource={product2}
-        />
-        <Product
-          description={{
-            brand: "adidas",
-            name: "Cartoon Astronaut T-Shirts",
-            price: 78,
-          }}
-          productImgSource={product3}
-        />
-        <Product
-          description={{
-            brand: "adidas",
-            name: "Cartoon Astronaut T-Shirts",
-            price: 78,
-          }}
-          productImgSource={product4}
-        />
-        <Product
-          description={{
-            brand: "adidas",
-            name: "Cartoon Astronaut T-Shirts",
-            price: 78,
-          }}
-          productImgSource={product5}
-        />
-        <Product
-          description={{
-            brand: "adidas",
-            name: "Cartoon Astronaut T-Shirts",
-            price: 78,
-          }}
-          productImgSource={product6}
-        />
-        <Product
-          description={{
-            brand: "adidas",
-            name: "Cartoon Astronaut T-Shirts",
-            price: 78,
-          }}
-          productImgSource={product7}
-        />
-        <Product
-          description={{
-            brand: "adidas",
-            name: "Cartoon Astronaut T-Shirts",
-            price: 78,
-          }}
-          productImgSource={product8}
-        />
+        <Product2 imgSource={product1} productObject={products[8]} />
+        <Product2 imgSource={product2} productObject={products[9]} />
+        <Product2 imgSource={product3} productObject={products[10]} />
+        <Product2 imgSource={product4} productObject={products[11]} />
+        <Product2 imgSource={product5} productObject={products[12]} />
+        <Product2 imgSource={product6} productObject={products[13]} />
+        <Product2 imgSource={product7} productObject={products[14]} />
+        <Product2 imgSource={product8} productObject={products[15]} />
       </div>
     </section>
   );
