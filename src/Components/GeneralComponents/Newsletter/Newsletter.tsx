b6d58e6fbfe69e8604e7d40b26f8971932d994cd
Gregory <grzesb1508@wp.pl>
2025-04-03 18:58:52 +0200

    Added Newsletter post request to API and styled it

--- src/Components/GeneralComponents/Newsletter/Newsletter.tsx
+++ src/Components/GeneralComponents/Newsletter/Newsletter.tsx
@@ -1,12 +1,23 @@
 import { useForm } from "react-hook-form";
 import { useCreateConsumerEmailQuery } from "../../../queries/useCreateConsumerEmailQuery";
 import styles from "./styles.module.scss";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { clientEmailSchema } from "../../../validation";
+import { UserEmailDto } from "../../../types";
 
 export const Newsletter = () => {
   const { mutate } = useCreateConsumerEmailQuery();
-  const {} = useForm({
-    // resolver: yupResolver
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitSuccessful },
+  } = useForm({
+    resolver: yupResolver(clientEmailSchema),
   });
+
+  const onFormSubmit = (data: UserEmailDto) => {
+    mutate(data);
+  };
 
   return (
     <section className={styles.newsletter}>
@@ -17,13 +28,27 @@ export const Newsletter = () => {
           <span className={styles.newsletter__strong}> special offers</span>
         </p>
       </div>
-      <form className={styles.newsletter__form}>
+      <form
+        className={styles.newsletter__form}
+        onSubmit={handleSubmit(onFormSubmit)}
+      >
         <input
+          {...register("email")}
           className={styles.newsletter__form__input}
           type="text"
           placeholder="Your email address"
         />
-        <button className={styles.newsletter__form__button}>Sign Up</button>
+        <button className={styles.newsletter__form__button} type="submit">
+          Sign Up
+        </button>
+        {errors ? (
+          <p className={styles.newsletter__error}>{errors.email?.message}</p>
+        ) : null}
+        {isSubmitSuccessful ? (
+          <p className={styles.newsletter__success}>
+            You have successfully signed to our newsletter!
+          </p>
+        ) : null}
       </form>
     </section>
   );
--- src/validation/index.ts
+++ src/validation/index.ts
@@ -1,7 +1,7 @@
 import { object, string } from "yup";
 
 export const clientEmailSchema = object({
-  Email: string()
+  email: string()
     .required("E-mail is required in order to log in properly!")
     .matches(
       /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
