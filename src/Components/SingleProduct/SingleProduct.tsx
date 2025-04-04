f7c5a61ab49ba12c63b806a99819804c31e2a5b0
Gregory <grzesb1508@wp.pl>
2025-04-04 21:45:38 +0200

    Removed unnecessary styles file

--- src/Components/SingleProduct/SingleProduct.tsx
+++ src/Components/SingleProduct/SingleProduct.tsx
@@ -3,7 +3,6 @@ import { Header } from "../GeneralComponents/Header/Header";
 import { Newsletter } from "../GeneralComponents/Newsletter/Newsletter";
 import { FeaturedProducts } from "../Home/FeaturedProducts/FeaturedProducts";
 import { ProductDetails } from "./ProductDetails/ProductDetails";
-import styles from "./styles.module.scss";
 
 export const SingleProduct = () => {
   return (
